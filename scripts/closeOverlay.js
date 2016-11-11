0982e63f08ca8374f6753bf276b25a8a8d2408ca
0x24d <daniel.longfield@tuta.io>
2016-11-11 23:29:39 +0000

    Mobile overlay working, update font-awesome to 4.7.0

---
+++ scripts/closeOverlay.js
@@ -0,0 +1,5 @@
+function closeOverlay(){
+document.getElementById("openOverlay").style.display="block"
+document.getElementById("mobileOverlay").style.display="none"
+document.getElementById("closeOverlay").style.display="none";
+}
---
+++ scripts/openOverlay.js
@@ -0,0 +1,5 @@
+function openOverlay(){
+document.getElementById("openOverlay").style.display="none"
+document.getElementById("mobileOverlay").style.display="block"
+document.getElementById("closeOverlay").style.display="block";
+}
