80bbb839fe5086dc5d0270b564cf20fc53d44260
0x24d <daniel.longfield@tuta.io>
2016-11-12 00:16:33 +0000

    move js into single file

--- scripts/closeOverlay.js
+++
@@ -1,5 +0,0 @@
-function closeOverlay(){
-document.getElementById("openOverlay").style.display="block"
-document.getElementById("mobileOverlay").style.display="none"
-document.getElementById("closeOverlay").style.display="none";
-}
--- scripts/overlay.js
+++ scripts/overlay.js
@@ -3,3 +3,8 @@ document.getElementById("openOverlay").style.display="none"
 document.getElementById("mobileOverlay").style.display="block"
 document.getElementById("closeOverlay").style.display="block";
 }
+function closeOverlay(){
+document.getElementById("openOverlay").style.display="block"
+document.getElementById("mobileOverlay").style.display="none"
+document.getElementById("closeOverlay").style.display="none";
+}
