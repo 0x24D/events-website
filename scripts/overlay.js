1db83b10e1af9d8e8cbbabfd51e0908bfbc9ea34
0x24d <daniel.longfield@tuta.io>
2016-11-12 01:08:59 +0000

    media queries added, seperated css files

--- scripts/overlay.js
+++ scripts/overlay.js
@@ -1,10 +1,11 @@
 function openOverlay(){
-document.getElementById("openOverlay").style.display="none"
-document.getElementById("mobileOverlay").style.display="block"
-document.getElementById("closeOverlay").style.display="block";
+    document.getElementById("openOverlay").style.display="none"
+    document.getElementById("navLinks").style.display="block"
+    document.getElementById("closeOverlay").style.display="block";
 }
+
 function closeOverlay(){
-document.getElementById("openOverlay").style.display="block"
-document.getElementById("mobileOverlay").style.display="none"
-document.getElementById("closeOverlay").style.display="none";
+    document.getElementById("openOverlay").style.display="block"
+    document.getElementById("navLinks").style.display="none"
+    document.getElementById("closeOverlay").style.display="none";
 }
