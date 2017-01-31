8fc7a4691e849cc009e4b549d5a402efa5379cbb
0x24d <daniel.longfield@tuta.io>
2017-01-31 20:06:21 +0000

    Change colour scheme, start event search form

--- includes/events.inc.php
+++ includes/events.inc.php
@@ -1,9 +1,36 @@
 <?php
-$sql= "SELECT city, area, country FROM cities";
-foreach($pdo->query($sql) as $row) {
-    echo "<tr>";
-    echo "<td>{$row['city']}</td>";
-    echo "<td>{$row['area']}</td>";
-    echo "<td>{$row['country']}</td>";
-    echo "</tr>";
+$countrysql= "SELECT distinct country FROM cities";
+$areasql= "SELECT distinct area FROM cities where country = :country";
+$citysql= "SELECT distinct city FROM cities where area = :area";
+?>
+<form action="index.php" method='post'>
+<select name="country">
+<option value"country" selected>Select a country</option>
+<?php
+foreach($pdo->query($countrysql) as $row) {
+    echo"<option value=\"{$row['country']}\">{$row['country']}</option>";
 }
+?>
+</select>
+
+<!--<select name="area" disabled>
+<option value"area" selected>Select an area</option>
+<?php
+foreach($pdo->query($areasql) as $row) {
+    echo"<option value={$row['area']}>{$row['area']}</option>";
+}
+?>
+</select>
+<select name="city"disabled>
+<option value"city" selected>Select a city</option>
+<?php
+foreach($pdo->query($citysql) as $row) {
+    echo"<option value={$row['city']}>{$row['city']}</option>";
+}
+?>
+</select> -->
+<input type="submit" name="search" value="Search">
+</form>
+<?php
+$selectedcountry = $_POST["country"];
+echo"Selected country: $selectedcountry" ?>
